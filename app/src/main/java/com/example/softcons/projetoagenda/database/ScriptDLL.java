ea60191f1c9afaae1cbd368b41e4809ed45216cf
RafaelSoftcons <seesystemm@gmail.com>
2018-06-18 16:02:39 -0300

    Criada a estrutura para a database

---
+++ app/src/main/java/com/example/softcons/projetoagenda/database/ScriptDLL.java
@@ -0,0 +1,18 @@
+package com.example.softcons.projetoagenda.database;
+
+public class ScriptDLL {
+
+    public static String getCreateTableAgenda(){
+        StringBuilder sql = new StringBuilder();
+
+        sql.append("    CREATE TABLE IF NOT EXISTS AGENDA (");
+        sql.append("        CODIGO INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,");
+        sql.append("        DESCRICAO        VARCHAR(250) NOT NULL DEFAULT(''),");
+        sql.append("        TIPO    VARCHAR(255) NOT NULL DEFAULT(''),");
+        sql.append("        HORA       VARCHAR(250) NOT NULL DEFAULT(''),");
+        sql.append("        DATA    VARCHAR(200) NOT NULL DEFAULT('') )");
+
+        return sql.toString();
+    }
+
+}
